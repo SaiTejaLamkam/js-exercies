bed8dcbcd8407c8cfdd49fc90c71127effc32b59
Sai Teja Lamkam <28135010+SaiTejaLamkam@users.noreply.github.com>
2023-03-10 12:43:29 +0530

    JS Exercise

---
+++ index.js
@@ -0,0 +1,85 @@
+
+// Exercise: #1
+// create two objects, each with a firstName, lastName, mass, height, and calcBMI method
+// calculate the BMI of each object and store it in the object
+// log to the console who has the highest BMI, together with the full name and the respective BMI
+
+// Test Data
+// mass = 78 kg, height = 1.69 m -> Mark Miller
+// mass = 92 kg, height = 1.95 m -> John Smith
+
+// BMI = mass/height ** 2 = mass/(height * height) (mass in kg and height in meter)
+
+
+// Exercise #2
+// create a function that will calculate the total amount of the bill and the tip
+// create a function that will calculate the average of the tips
+// create a function that will calculate the average of the total amount of the bill and the tip
+// bill <= 50 = Tip 15%
+// bill > 50 = Tip 20%
+
+// Test Data
+// bills = [124, 48, 268, 180, 42]
+
+
+// Exercise #3
+// Write a function that take an array and returns and counts the truthy values in the array.
+// Falsy values are: false, 0, '', null, undefined, NaN
+// Truthy values are: true, 1, 'a', [], {}, 'false'
+// Example: countTruthy([0, null, '', 'Hello', 1, 2, 3]) => 4
+
+// Exercise #4
+// Write a function that take an object as an argument and display only the properties with the type string.
+// Example: showProperties({name: 'John', age: 30, address: '123 Main St'}) => name, address
+
+
+// Exercise #5
+// Write a function called calculateGrade that takes an array of marks as an argument and returns the average of the marks.
+// If the average is less than 60 return F
+// If the average is less than 70 return D
+// If the average is less than 80 return C
+// If the average is less than 90 return B
+// If the average is less than 100 return A
+// Example: calculateGrade([80, 80, 50]) => C
+
+
+/* // Array
+const companies = [
+{ name: "Company One", category: "Finance", start: 1981, end: 2004 },
+{ name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+{ name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+{ name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+{ name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+{ name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+{ name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+{ name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+{ name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+];
+
+// object
+obj = const person = {
+name: "Costas",
+"name":"Ram",
+"age":27,
+city: 'hyderabad,
+country: India
+};
+
+// Nested Object
+nested obj: var person = {
+"name":"Ram",
+"age":27,
+"vehicles": {
+"car":"limousine",
+"bike":"ktm-duke",
+"plane":"lufthansa"
+}
+} */
+
+
+
+// Exercise #6: In index.js create a function called sum that takes 2 arguments type numbers and returns their sum. If you do not provide the second argument then it will use the value of the first plus 1
+//Exercise #7: In index.js Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one
+//Exercise #8:In index.js Write a JavaScript program to filter out the specified values from a specified array. Return the original array without the filtered values
+//Exercise #9:In index.js write a programm that check if a value is null or undefined and returns a string saying what it is
+//Exercise #10:Write a JavaScript program to target a given value in a nested JSON object, based on the given key
